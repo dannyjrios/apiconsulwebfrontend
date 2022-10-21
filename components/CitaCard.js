10b55e7cc9181389ae1d41dd2fcc0db342dc2150
dannyjrios <113950486+dannyjrios@users.noreply.github.com>
2022-10-20 20:19:20 -0500

    Ajustar formulario citas
    Agregando estilos y etiquetas

--- components/CitaCard.js
+++ components/CitaCard.js
@@ -5,35 +5,39 @@ const CitaCard=(cita, micita)=>
 `
     <div class="card mt-3 mb-3">
         <div class="card-header mi-card">
-            <div>
+            <div >
                 ${cita.asignado ? `<div class="mi-badge-asignado">Asignada</div>` : `<div class="mi-badge-pendiente">Pendiente</div>`}
             </div>
             ${ micita ?
             `<div>
-                <a href="" class="btn btn-primary btn-sm me-2">Editar</a>
+                <a href="" class="btn btn-primary btn-sm me-4">Editar</a>
                 ${EliminarCitaBoton()}
             </div>`
             :""
             }
         </div>
         <div class="card-body">
+            <div class="card-text float-md-right">
+                <strong>Fecha de la cita:</strong> ${cita.fecha}
+            </div>
             <div class="card-title">
-                <a href="/cita/${cita.id}">
-                   Paciente ${cita.usuario} con el  ${cita.medico}
+                <a href="/cita/${cita.id}"><br>
+                    <strong>Paciente:</strong> ${cita.usuario} <br><strong>Doctor(a):</strong>  ${cita.medico}
                 </a>
             </div>
             <div class="card-text">
-                Observaciones: ${cita.observacion}
+                <strong>Observaciones:</strong> ${cita.observacion}
             </div>
-            <div class="card-text">
-                Fecha: ${cita.fecha}
-            </div>
-            <div class="card-text">
-                ${cita.usuario}, ${cita.creado}
+            <!-- <div class="card-text">
+                <strong>Fecha de la cita:</strong> ${cita.fecha}
+            </div> -->
+            <div class="card-text" style="font-size: 13px;">
+                <strong> Creado por</strong>  ${cita.usuario} <strong>  el </strong>  ${cita.creado}
             </div>
 
         </div>
     </div>
+    
 `
 
 
